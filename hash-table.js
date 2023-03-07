d554a3ffdb612b7a331be1ab11ff4f6ce0e23610
Nick <nick.buscemi13@gmail.com>
2023-03-07 11:19:58 -0500

    part1finished

--- hash-table.js
+++ hash-table.js
@@ -11,15 +11,21 @@ class KeyValuePair {
 class HashTable {
 
   constructor(numBuckets = 4) {
-    // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
+    
   }
 
   hash(key) {
     // Your code here
+    let hex = sha256(key).slice(0, 8);
+    return parseInt(hex, 16);
   }
 
   hashMod(key) {
     // Your code here
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
